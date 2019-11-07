d4b4da6e51bfae0cc23dd7eaf94ad8bc7101c893
Stephen Cousins <stephen@ciandt.com>
2019-11-07 09:51:24 -0300

    New profile to publish the container to the registry

--- KubernetesPoC/Startup.cs
+++ KubernetesPoC/Startup.cs
@@ -13,12 +13,15 @@ namespace KubernetesPoC
 {
 	public class Startup
 	{
+		protected const string ENDPOINT_HEALTH_CHECKS = "/health";
+		protected const string CONNECTION_STRING_ALBUM_CONTEXT = "AlbumContext";
+
+		public IConfiguration Configuration { get; }
+
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
-
-		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
@@ -31,12 +34,18 @@ namespace KubernetesPoC
 
 			// Entity Framework
 			services.AddDbContextPool<MusicContext>(
-				options => options.UseSqlServer(Configuration.GetConnectionString("AlbumContext")));
+				options => options.UseSqlServer(Configuration.GetConnectionString(CONNECTION_STRING_ALBUM_CONTEXT)));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			//var builder = new ConfigurationBuilder()
+			//	.SetBasePath(env.ContentRootPath)
+			//	.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+			//	.AddJsonFile("secrets/appsettings.secrets.json", optional: true)
+			//	.AddEnvironmentVariables();
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
@@ -49,7 +58,7 @@ namespace KubernetesPoC
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
-				endpoints.MapHealthChecks("/health");
+				endpoints.MapHealthChecks(ENDPOINT_HEALTH_CHECKS);
 			});
 		}
 	}
