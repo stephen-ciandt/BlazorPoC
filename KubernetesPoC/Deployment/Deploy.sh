364665b5825bac4a112fd2a772d1c61460609270
Stephen Cousins <stephen@ciandt.com>
2019-11-07 17:15:00 -0300

    Pointed to newly created container registry and added CLI support for deployment from the registry to the cluster

---
+++ KubernetesPoC/Deployment/Deploy.sh
@@ -0,0 +1,10 @@
+﻿az login
+az aks get-credentials --resource-group DEV --name panasonic-dev
+
+kubectl create secret docker-registry panasonicdevcrsecret
+  --docker-server panasonicdevcr.azurecr.io
+  --docker-email stephen@ciandt.com
+  --docker-username panasonicdevcr
+  --docker-password <password>
+
+kubectl apply -f kubernetespoc.yaml
