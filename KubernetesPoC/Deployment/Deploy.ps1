3eede7f69dcf0b8778cfefdc861e70477af684c7
Stephen Cousins <stephen@ciandt.com>
2019-11-11 16:09:51 -0300

    .

--- KubernetesPoC/Deployment/Deploy.ps1
+++ KubernetesPoC/Deployment/Deploy.ps1
@@ -1,10 +1,10 @@
 ﻿az login
 az aks get-credentials --resource-group DEV --name panasonic-dev
 
-kubectl create secret docker-registry panasonicdevcrsecret
-  --docker-server panasonicdevcr.azurecr.io
-  --docker-email stephen@ciandt.com
-  --docker-username panasonicdevcr
-  --docker-password <password>
+kubectl create secret docker-registry panasonicdevcrsecret `
+  --docker-server panasonicdevcr.azurecr.io `
+  --docker-email stephen@ciandt.com `
+  --docker-username panasonicdevcr `
+  --docker-password k8vStysiT3iX0gGg/1ZEecWVnBMMx4pQ
 
 kubectl apply -f kubernetespoc.yaml
